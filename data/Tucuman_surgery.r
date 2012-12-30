adf15a7bf8efaa28dcf15a7a520e44ea8d6b448f
Prof. Elias Carvalho <ecacarva2@gmail.com>
2012-12-30 18:03:11 -0200

    Elias update data

---
+++ data/Tucuman_surgery.r
@@ -0,0 +1,11 @@
+#In the case of hojas 1 and 2 have differents patients.
+
+
+
+surg1 <- read.csv("C:/Users/Luciano/Google Drive/TUCUMAN/Cirurgia Tucuman/data/surgery_1.csv", sep=";")
+surg1
+surg2 <- read.csv("C:/Users/Luciano/Google Drive/TUCUMAN/Cirurgia Tucuman/data/surgery_2.csv", sep=";")
+surg2
+inhmort <- read.table("C:/Users/Luciano/Google Drive/TUCUMAN/Cirurgia Tucuman/data/Death_Tucuman.csv", header=T, quote="\"")
+mortality2 <- read.csv("C:/Users/Luciano/Google Drive/TUCUMAN/Cirurgia Tucuman/data/mortality2.csv", sep=";")
+mortality2
